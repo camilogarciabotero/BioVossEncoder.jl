1b38f0a3955c1a5fce08cecc025f348a54603d05
Camilo García <ca.garcia2@uniandes.edu.co>
2023-12-26 15:05:51 -0500

    Exported sequence_binarizer function and removed unnecessary export

--- src/BioVossEncoder.jl
+++ src/BioVossEncoder.jl
@@ -14,7 +14,10 @@ using PrecompileTools: @setup_workload, @compile_workload
 using TestItemRunner: @testitem
 
 include("types.jl")
-export BinarySequenceMatrix, BSM, binary_sequence_matrix
+export BinarySequenceMatrix, BSM
+
+include("utils.jl")
+export binary_sequence_matrix, sequence_binarizer
 
 @setup_workload begin
     # Putting some things in `@setup_workload` instead of `@compile_workload` can reduce the size of the
