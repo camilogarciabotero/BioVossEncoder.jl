7abab17a57d27afcd932ec9b3d5eac28445bcd10
Camilo García <ca.garcia2@uniandes.edu.co>
2024-10-20 01:40:44 -0500

    Add a pfm implementation

--- src/utils.jl
+++ src/utils.jl
@@ -1,3 +1,5 @@
+export vossvector, vossmatrix, pfm
+
 """
     vossvector(sequence::NucleicSeqOrView{A}, molecule::T) where {A <: NucleicAcidAlphabet, T <: BioSymbol}
     vossvector(sequence::SeqOrView{AminoAcidAlphabet}, molecule::T) where {T <: BioSymbol}
@@ -113,8 +115,24 @@ function vossmatrix(sequence::SeqOrView{AminoAcidAlphabet})
    return bm
 end
 
-export pfm
+"""
+    pfm(v::Vector{T}) where {T <: SeqOrView{<:Alphabet}}
 
+Calculate the position frequency matrix (PFM) for a given vector of sequences or sequence views.
+
+# Arguments
+- `v::Vector{T}`: A vector of sequences or sequence views, where each element is of type `T` which is a subtype of `SeqOrView` parameterized by an `Alphabet`.
+
+# Returns
+- A matrix representing the position frequency matrix (PFM) of the input sequences.
+
+# Details
+- The function first creates a copy of the input vector `v`.
+- It then determines the sequence with the maximum length (`vmax`) and removes it from the vector.
+- The function computes the Voss matrix for each sequence in the vector.
+- Finally, it sums the Voss matrices and returns the result as the position frequency matrix.
+
+"""
 function pfm(v::Vector{T}) where {T <: SeqOrView{<:Alphabet}}
     vc = copy(v)
     vmax =  all(i -> length(i) == length(vc[1]), vc) ? popat!(vc, 1) : popat!(vc, findmax(length, v)[2])
