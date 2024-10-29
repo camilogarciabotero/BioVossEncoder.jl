325255200ccc766b07a9bce6008f28c2dcf0c34f
Camilo García <ca.garcia2@uniandes.edu.co>
2024-10-28 20:47:39 -0500

    Refactor repository URL in make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -25,7 +25,7 @@ DocMeta.setdocmeta!(BioVossEncoder, :DocTestSetup, :(using BioVossEncoder); recu
 # deploydocs(; repo = "https://github.com/camilogarciabotero/BioVossEncoder.jl")
 
 fmt = DocumenterVitepress.MarkdownVitepress(
-    repo = "https://github.com/camilogarciabotero/BioMarkovChains.jl/",
+    repo = "https://github.com/camilogarciabotero/BioVossEncoder.jl/",
     devbranch = "main",
 )
 
